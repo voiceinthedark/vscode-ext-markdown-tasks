82f0e5b3e92242845767f2ac964fdf61ffc23070
voiceinthedark <darkrisingforce@gmail.com>
2023-06-30 19:56:06 +0300

    Added stats and functionality to insert stats at cursor position

--- extension.js
+++ extension.js
@@ -9,45 +9,70 @@ const vscode = require('vscode');
  * @param {vscode.ExtensionContext} context
  */
 function activate(context) {
+  // Use the console to output diagnostic information (console.log) and errors (console.error)
+  // This line of code will only be executed once when your extension is activated
+  console.log(
+    'Congratulations, your extension "markdown-task-calculator" is now active!'
+  );
 
-	// Use the console to output diagnostic information (console.log) and errors (console.error)
-	// This line of code will only be executed once when your extension is activated
-	console.log('Congratulations, your extension "markdown-task-calculator" is now active!');
+  // The command has been defined in the package.json file
+  // Now provide the implementation of the command with  registerCommand
+  // The commandId parameter must match the command field in package.json
+  let disposable = vscode.commands.registerCommand(
+    'markdown-task-calculator.calculateTasks',
+    function () {
+      let totalTaskCount;
+      let finishedTaskCount;
 
-	// The command has been defined in the package.json file
-	// Now provide the implementation of the command with  registerCommand
-	// The commandId parameter must match the command field in package.json
-	let disposable = vscode.commands.registerCommand('markdown-task-calculator.calculateTasks', function () {
-		// The code you place here will be executed every time your command is executed
+      let editor = vscode.window.activeTextEditor;
 
-		let totalTaskCount;
-		let finishedTaskCount;
+      if (editor) {
+        // get the current document
+        const document = editor.document;
+        const selection = editor.selection;
+        // get all the lines in the current document
+        const lines = document.getText().split('\n');
+        console.log(lines.length);
+        // loop over the lines array and filter the lines that starts with - [ ] or - [x]
+        totalTaskCount = lines.filter((line) => {
+          return (
+            line.trim().startsWith('- [ ]') || line.trim().startsWith('- [x]')
+          );
+        }).length;
 
-		// get the current document
-		const document = vscode.window.activeTextEditor.document;
-		// get all the lines in the current document
-		const lines = document.getText().split('\n');
-		console.log(lines.length);
-		// loop over the lines array and filter the lines that starts with - [ ] or - [x]
-		totalTaskCount = lines.filter((line) => {
-			return line.trim().startsWith('- [ ]') || line.trim().startsWith('- [x]');
-		}).length;
+        finishedTaskCount = lines.filter((line) => {
+          return line.trim().startsWith('- [x]');
+        }).length;
 
-		finishedTaskCount = lines.filter((line) => {
-			return line.trim().startsWith('- [x]');
-		}).length;
+        // display the result at current position
+        editor.edit((editBuilder) => {
+          editBuilder.insert(
+            selection.active,
+            '\n**Total Task Count:** ' +
+              totalTaskCount +
+              '\n**Finished Task Count:** ' +
+              finishedTaskCount +
+              '\n**Completion Percentage:** ' +
+              ((finishedTaskCount / totalTaskCount) * 100).toFixed(2) +
+              '%'
+          );
+        });
 
-		// Display a message box to the user
-		vscode.window.showInformationMessage(`Total Task Count: ${totalTaskCount} Finished Task Count: ${finishedTaskCount}`);
-	});
+        // Display a message box to the user
+        vscode.window.showInformationMessage(
+          `Total Task Count: ${totalTaskCount} Finished Task Count: ${finishedTaskCount}`
+        );
+      }
+    }
+  );
 
-	context.subscriptions.push(disposable);
+  context.subscriptions.push(disposable);
 }
 
 // This method is called when your extension is deactivated
 function deactivate() {}
 
 module.exports = {
-	activate,
-	deactivate
-}
+  activate,
+  deactivate,
+};
